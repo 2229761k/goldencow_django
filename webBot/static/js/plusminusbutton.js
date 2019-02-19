c9e762d6106a6763a380f80559a8d4296b5d2d86
2229761k <doblyhyung@gmail.com>
2019-02-19 13:11:15 +0900

    state value

--- webBot/static/js/plusminusbutton.js
+++ webBot/static/js/plusminusbutton.js
@@ -1,63 +1,102 @@
-$(document).ready(function () {
-    //onchange of rooms-count
-    $(document).on('change', '#rooms', function () {
-        var roomsSelected = $('#rooms option:selected').val();
-        var roomsDisplayed = $('[id^="room-"]:visible').length;
-        var roomsRendered = $('[id^="room-"]').length;
-        var orig = $('#peoplerooms').find(roomsSelected);
 
-        //if room count is greater than number displayed - add or show accordingly
-        if (roomsSelected > roomsDisplayed) {
-
-            for (var i = 1; i <= roomsSelected; i++) {
-                var r = $('#room-' + i);
-                if (r.length == 0) {
-
-                    var clone = $('#room-1').clone(); //clone
-                    clone.children(':first').text("Room " + i);
-                    //change ids appropriately
-                    setNewID(clone, i);
-                    clone.children('div').children('select').each(function () {
-                        setNewID($(this), i);
-                    });
-                    $(clone).insertAfter($('#room-' + roomsRendered));
-
-                } else {
-                    //if the room exists and is hidden 
-                    $(r).show();
-                }
-            }
-
-        } else {
-            //else if less than room count selected - hide
-            for (var i = ++roomsSelected; i <= roomsRendered; i++) {
-                $('#room-' + i).hide();
-            }
-        }
-
-    });
-
-});
 
 
 // TOP CONTROLS
 $('.plus').click(function (e) {
     e.preventDefault();
-    var sp = parseFloat($(this).prev('span').text());
-    if(sp<100){
-        $(this).prev('span').text(sp + 1);
-    }else{
-        $(this).next('span').text(100);
+    var sp_bithumb = parseFloat($(this).prev('#roomcount1').text());
+    var sp_upbit = parseFloat($(this).prev('#roomcount2').text());
+    var sp_binance = parseFloat($(this).prev('#roomcount3').text());
+
+    if(sp_bithumb<100){
+        $(this).prev('#roomcount1').text(sp_bithumb + 1);
+    }
+    else{
+        $(this).next('#roomcount1').text(100);
+    }
+    if(sp_upbit<100){
+        $(this).prev('#roomcount2').text(sp_upbit + 1);
+    }
+    else{
+        $(this).next('#roomcount2').text(100);
+    }
+    if(sp_binance<100){
+        $(this).prev('#roomcount3').text(sp_binance + 1);
+    }
+    else{
+        $(this).next('#roomcount3').text(100);
     }
 });
 
 $('.minus').click(function (e) {
     e.preventDefault();
-    var sp = parseFloat($(this).next('span').text());
-    $(this).next('span').text(sp - 1);
-    if (!isNaN(sp) && sp > 0) {
-        $(this).next('span').text(sp - 1);
-    } else {
-        $(this).next('span').text(0);
+    var sp_bithumb = parseFloat($(this).next('#roomcount1').text());
+    var sp_upbit = parseFloat($(this).next('#roomcount2').text());
+    var sp_binance = parseFloat($(this).next('#roomcount3').text());
+
+    if(!isNaN(sp_bithumb) && sp_bithumb>0){
+        $(this).next('#roomcount1').text(sp_bithumb - 1);
+    }
+    else {
+        $(this).next('#roomcount1').text(0);
+    }
+    if(!isNaN(sp_upbit) && sp_upbit>0){
+        $(this).next('#roomcount2').text(sp_upbit - 1);
+    }
+    else {
+        $(this).next('#roomcount2').text(0);
+    }
+    if(!isNaN(sp_binance) && sp_binance>0){
+        $(this).next('#roomcount3').text(sp_binance - 1);
+    }
+    else {
+        $(this).next('#roomcount3').text(0);
+    }
+});
+
+
+
+var isBithumb=false;
+var isUpbit=false;
+var isBinance=false;
+
+$('.switch').click(function(e){
+    e.preventDefault();
+    var bithumb_button = $('#myButton1');
+   
+    if(isBithumb && bithumb_button)
+    {
+        isBithumb=false;
+        bithumb_button.text('OFF');   
+    }else{
+        isBithumb=true;
+        bithumb_button.text('ON'); 
+    }
+          
+});
+
+$('.switch2').click(function(e){
+    e.preventDefault();
+    var upbit_button = $('#myButton2');
+    if(isUpbit && upbit_button)
+    {
+        isUpbit=false;
+        upbit_button.text('OFF');   
+    }else{
+        isUpbit=true;
+        upbit_button.text('ON'); 
+    }
+});
+
+$('.switch3').click(function(e){
+    e.preventDefault();
+    var binance_button = $('#myButton3');
+    if(isBinance && binance_button)
+    {
+        isBinance=false;
+        binance_button.text('OFF');   
+    }else{
+        isBinance=true;
+        binance_button.text('ON'); 
     }
 });
